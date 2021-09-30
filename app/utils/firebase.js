7393a27368b0807d7253c13d33067c905afee6de
KevinLuna17 <44761951+KevinLuna17@users.noreply.github.com>
2021-09-30 12:52:52 -0500

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgVbCou55F4CwHKIhPbJKp0z1TYIdKfFQ",
-  authDomain: "rn-uber-eats-clone-kl.firebaseapp.com",
-  projectId: "rn-uber-eats-clone-kl",
-  storageBucket: "rn-uber-eats-clone-kl.appspot.com",
-  messagingSenderId: "1036324854718",
-  appId: "1:1036324854718:web:fad66655982eb25a2ff7ef",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
